aca27256b98b09f8a74f55f17aa7bcf114749db4
fernando <fernando@192.168.1.9>
2016-08-28 09:18:38 +0800

    Admin first release of Admin portlets

---
+++ docroot/WEB-INF/src/sg/com/para/intranet/portlet/PayrollAdmin.java
@@ -0,0 +1,11 @@
+package sg.com.para.intranet.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PayrollAdmin
+ */
+public class PayrollAdmin extends MVCPortlet {
+ 
+
+}
